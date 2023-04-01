d9cea61069897ef493ae38056ffacdbaa387ca46
raw <ryaboff99@gmail.com>
2023-04-02 02:24:05 +0300

    Added List Beers endpoint. Closes #3
    * Add BeerController
    * Add BeerControllerTest
    * Add BeerService
    * Add BeerServiceImpl
    * Update application.properties - add 'debug' logging level

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+public class BeerController {
+    private final BeerService beerService;
+
+    @GetMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
+
+}
