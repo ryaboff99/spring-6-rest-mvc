02cef4ed2038fb6ca05df092906e33694d70eb8d
raw <ryaboff99@gmail.com>
2023-04-09 20:52:20 +0300

    Added test for listBeers() method of Beer Controller. #17
    * Add allBeersAreListed() method to BeerControllerTest.java

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6restmvc.controller;
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import guru.springframework.spring6restmvc.services.BeerServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -29,7 +30,8 @@ class BeerControllerTest {
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() throws Exception {
+    @DisplayName("Beer is returned by Id")
+    void beerIsReturnedById() throws Exception {
         Beer testBeer = beerServiceImpl.getAllBeers().get(0);
 
         given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
@@ -40,5 +42,17 @@ class BeerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
+    }
+
+    @Test
+    @DisplayName("All Beers are Listed")
+    void allBeersAreListed() throws Exception {
+        given(beerService.getAllBeers()).willReturn(beerServiceImpl.getAllBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType((MediaType.APPLICATION_JSON)))
+                .andExpect(jsonPath("$.length()", is(3)));
     }
 }
