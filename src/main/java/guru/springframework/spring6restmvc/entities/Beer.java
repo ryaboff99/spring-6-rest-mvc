e6991b4ff826362a0cb65294fbffc2c6ac47420b
raw <ryaboff99@gmail.com>
2023-05-31 01:21:25 +0300

    Added JPA level validation for Beer. Closes #41
    * Add validation constraints to Beer.java
    * Add repositorySaveBeerWithInvalidValueThrowsException() method to BeerRepositoryTest.java
    * Update repositorySaveBeer() method in BeerRepositoryTest.java

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -6,12 +6,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -27,14 +32,28 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     @Version
     private Integer version;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 255)
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,12 +1,17 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @DisplayName("Beer Repository Tests")
@@ -16,10 +21,34 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    @DisplayName("Repository save Beer with invalid value throws exception")
+    void repositorySaveBeerWithInvalidValueThrowsException() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("My Beer 666666666666666666666666666666666666666666666666666666666666666666666666666666")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("123123")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
+
+    @Test
     @DisplayName("Repository save Beer")
     void repositorySaveBeer() {
-        Beer savedBeer = beerRepository.save(Beer.builder().beerName("My Beer").build());
-                assertThat(savedBeer).isNotNull();
-                assertThat(savedBeer.getId()).isNotNull();
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123123")
+                .price(new BigDecimal("11.99"))
+                .build());
+
+        beerRepository.flush();
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
     }
 }
