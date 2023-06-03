69961b5ef1ab5919e51ca7f0aa72833ca921621f
raw <ryaboff99@gmail.com>
2023-06-04 00:23:39 +0300

    Added JPA level validation for Customer. Closes #44
    * Add validation constraints to Customer.java
    * Add repositorySaveCustomerWithInvalidValueThrowsException() method to CustomerRepositoryTest.java
    * Update repositorySaveCustomer() method in CustomerRepositoryTest.java

--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +29,9 @@ public class Customer {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
+    @NotNull
+    @NotBlank
     private String name;
     @Version
     private Integer version;
--- src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -1,12 +1,14 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Customer;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @DisplayName("Customer Repository Tests")
@@ -16,9 +18,23 @@ class CustomerRepositoryTest {
     CustomerRepository customerRepository;
 
     @Test
+    @DisplayName("Repository save Customer with invalid value throws exception")
+    void repositorySaveCustomerWithInvalidValueThrowsException() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            customerRepository.save(Customer.builder().name("").build());
+
+            customerRepository.flush();
+        });
+    }
+
+    @Test
     @DisplayName("Repository save Customer")
     void repositorySaveCustomer() {
         Customer customer = customerRepository.save(Customer.builder().name("New Name").build());
+
+        customerRepository.flush();
+
+        assertThat(customer.getId()).isNotNull();
         assertThat(customer.getId()).isNotNull();
     }
 }
