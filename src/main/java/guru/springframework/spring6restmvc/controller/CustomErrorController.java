5c9ebbbc21c12ee82c312133c44c337dcb482334
raw <ryaboff99@gmail.com>
2023-06-03 13:17:56 +0300

    Added validation Exception Handler. Closes #43
    * Add CustomErrorController.java

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class CustomErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
+
+        List errorList = exception.getFieldErrors().stream()
+                .map(fieldError ->
+                    Map.of(fieldError.getField(), fieldError.getDefaultMessage())).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorList);
+    }
+}
